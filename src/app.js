5b397dd3994b90b59056090dd57d7ea618c19d0c
nidhisahani <nidhinishad42@gmail.com>
2023-12-10 19:49:43 +0530

    commit 1

--- src/app.js
+++ src/app.js
@@ -9,13 +9,12 @@ const subscriber = require('./models/subscribers')
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.set('view engine', 'ejs')
-
+app.use(express.static('./'));
 
 
 //Api to render index.ejs page
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index.html');
 });
 
 
